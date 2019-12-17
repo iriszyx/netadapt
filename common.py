1a765d44ec182b05ecbd4581b5a99cbf336c73e3
zhaoyuxin <zyx_iris@126.com>
2019-12-17 10:54:33 +0800

    add worker-related filenames for different devices

--- common.py
+++ common.py
@@ -7,3 +7,10 @@ WORKER_ACCURACY_FILENAME_TEMPLATE = 'iter_{}_block_{}_accuracy.txt'
 WORKER_RESOURCE_FILENAME_TEMPLATE = 'iter_{}_block_{}_resource.txt'
 WORKER_LOG_FILENAME_TEMPLATE = 'iter_{}_block_{}_log.txt'
 WORKER_FINISH_FILENAME_TEMPLATE = 'iter_{}_block_{}_finish.signal'
+
+# Worker-related filenames for different devices.
+WORKER_DEVICE_MODEL_FILENAME_TEMPLATE = 'iter_{}_block_{}_device_{}_model.pth.tar'
+WORKER_DEVICE_ACCURACY_FILENAME_TEMPLATE = 'iter_{}_block_{}_device_{}_accuracy.txt'
+WORKER_DEVICE_RESOURCE_FILENAME_TEMPLATE = 'iter_{}_block_{}_device_{}_resource.txt'
+WORKER_DEVICE_LOG_FILENAME_TEMPLATE = 'iter_{}_block_{}_device_{}_log.txt'
+WORKER_DEVICE_FINISH_FILENAME_TEMPLATE = 'iter_{}_block_{}_device_{}_finish.signal'
